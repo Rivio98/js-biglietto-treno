94475b60fc6aa7d84e5a7ecece9aa23152f2780b
Rivio98 <rivaldogjoni1998@gmail.com>
2024-06-25 16:18:44 +0200

    [UPD] Created const for user age

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 // creo una costante per i km  che l'utente deve fare, ai quali assegno il valore restituito da un prompt
 const km = prompt('Quanti km devi percorrere?')
+
+//  creo una costante per l'età dell'utente , al quale assegno il valore restituito da un prompt
+const age = prompt('Quanti anni hai?')
